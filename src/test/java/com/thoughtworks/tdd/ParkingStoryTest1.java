80030e756c487a07d256a38e3ef4f52f2f535f5f
LULI3 <luanyuanshizhu@126.com>
2019-07-14 14:32:34 +0800

    create and test should_return_two_right_car_when_pick_car_given_two_right_ticket

--- src/test/java/com/thoughtworks/tdd/ParkingStoryTest1.java
+++ src/test/java/com/thoughtworks/tdd/ParkingStoryTest1.java
@@ -19,4 +19,22 @@ public class ParkingStoryTest1 {
 
     }
 
+    @Test
+    public void should_return_two_right_car_when_pick_car_given_two_right_ticket(){
+        ParkingLot parkingLot = new ParkingLot(5);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Customer customer1 = new Customer(car1);
+        Customer customer2 = new Customer(car2);
+        customer1.parkCar(parkingBoy);
+        customer2.parkCar(parkingBoy);
+        customer1.pickCar(parkingBoy);
+        customer2.pickCar(parkingBoy);
+        assertEquals(customer1.getCar(),car1);
+        assertEquals(customer2.getCar(),car2);
+    }
+
+
+
 }
